01236e3abbd839a43171ac9f6e990cf2ad362d50
dave russo <d-russo@ti.com>
2015-02-17 21:57:01 -0800

    add cc3200 closure and tests

---
+++ src/bundles/energia/cc3200LP/tests/hello/hello.cpp
@@ -0,0 +1,20 @@
+/* BIOS Header files */
+#include <ti/sysbios/knl/Task.h>
+#include <xdc/runtime/System.h>
+
+/*
+ *  ======== main task ========
+ */
+Void hello_task(UArg arg0, UArg arg1)
+{
+    Int i;
+    
+    System_printf("hello_task(%p, %p) ...\n", arg0, arg1);
+    System_flush();
+
+    for (i = 0; ; i++) {
+        System_printf("%d: hello world\n", i);
+        System_flush();
+        Task_sleep(1000);
+    }
+}
