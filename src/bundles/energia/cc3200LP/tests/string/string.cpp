39c940be29bebc1b9f24a5fc12fdec585fe7f1a5
dave russo <d-russo@ti.com>
2015-03-22 00:14:49 -0700

    add class String test to cc3200LP

---
+++ src/bundles/energia/cc3200LP/tests/string/string.cpp
@@ -0,0 +1,29 @@
+#include <Energia.h> // Energia Wiring API
+#include <WString.h> // Energia Wiring API
+#include <xdc/runtime/System.h>
+
+// Pin   Function
+//  43 - red tri-color LED 02
+//  44 - green tri-color LED 02
+//  45 - blue tri-color LED 02
+//  46 - red LED 01
+int ledPin = 45;
+
+__extern void stringSetup(void)
+{
+    System_printf("stringSetup() ...");
+    System_flush();
+
+    String stringOne = String("Hello");
+    System_printf("len: %d\n", (stringOne + " String").length());
+
+    pinMode(ledPin, OUTPUT);  // set ledPin pin as output
+}
+
+__extern void stringLoop(void)
+{
+    digitalWrite(ledPin, HIGH);  // set the LED on
+    delay(1000);                 // wait for a second
+    digitalWrite(ledPin, LOW);   // set the LED off
+    delay(1000);                 // wait for a second
+}
