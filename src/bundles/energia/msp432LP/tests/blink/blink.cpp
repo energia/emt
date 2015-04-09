f4c03a4f859a690c88e3812665ace34dce6deaa9
dave russo <d-russo@ti.com>
2015-04-08 17:07:58 -0700

    enhanced basic tests provide slightly better coverage

--- src/bundles/energia/msp432LP/tests/blink/blink.cpp
+++ src/bundles/energia/msp432LP/tests/blink/blink.cpp
@@ -1,21 +1,28 @@
 #include <Energia.h> // Energia Wiring API
+#include <ti/drivers/Power.h>
 
 // Pin   Function
-//  43 - red tri-color LED 02
-//  44 - green tri-color LED 02
-//  45 - blue tri-color LED 02
-//  46 - red LED 01
-int ledPin = 45;
+//  73 - push button 1
+//  74 - push button 2
+//  75 - red tri-color LED 02
+//  76 - green tri-color LED 02
+//  77 - blue tri-color LED 02
+//  78 - yellow LED
+int ledPin = 77;
+
+int period = 0;
+int status = 0;
 
 __extern void blinkSetup(void)
 {
-    pinMode(ledPin, OUTPUT);  // set ledPin pin as output
+    pinMode(ledPin, OUTPUT);    // set ledPin pin as output
+    status = Power_setPerformanceLevel(0);
 }
 
 __extern void blinkLoop(void)
 {
-    digitalWrite(ledPin, HIGH);  // set the LED on
-    delay(1000);                 // wait for a second
-    digitalWrite(ledPin, LOW);   // set the LED off
-    delay(1000);                 // wait for a second
+    digitalWrite(ledPin, HIGH); // set the LED on
+    delay(period);              // wait for period ms
+    digitalWrite(ledPin, LOW);  // set the LED off
+    delay(1000 - period);       // wait for remainder of 1 second
 }
--- src/bundles/energia/msp432LP/tests/string/string.cpp
+++ src/bundles/energia/msp432LP/tests/string/string.cpp
@@ -1,29 +1,45 @@
 #include <Energia.h> // Energia Wiring API
 #include <WString.h> // Energia Wiring API
 #include <xdc/runtime/System.h>
+#include <xdc/runtime/Memory.h>
 
 // Pin   Function
-//  43 - red tri-color LED 02
-//  44 - green tri-color LED 02
-//  45 - blue tri-color LED 02
-//  46 - red LED 01
-int ledPin = 45;
+//  73 - push button 1
+//  74 - push button 2
+//  75 - red tri-color LED 02
+//  76 - green tri-color LED 02
+//  77 - blue tri-color LED 02
+//  78 - yellow LED
+int ledPin = 77;
+
+String stringOne = String("Hello: ");
 
 __extern void stringSetup(void)
 {
     System_printf("stringSetup() ...");
     System_flush();
 
-    String stringOne = String("Hello");
     System_printf("len: %d\n", (stringOne + " String").length());
 
-    pinMode(ledPin, OUTPUT);  // set ledPin pin as output
+    pinMode(ledPin, OUTPUT);    // set ledPin pin as output
 }
 
 __extern void stringLoop(void)
 {
-    digitalWrite(ledPin, HIGH);  // set the LED on
-    delay(1000);                 // wait for a second
-    digitalWrite(ledPin, LOW);   // set the LED off
-    delay(1000);                 // wait for a second
+    Memory_Stats stats;
+    static int count = 1;
+
+    digitalWrite(ledPin, HIGH); // set the LED on
+
+    System_printf("%s", (stringOne + count + '\n').c_str());
+    count++;
+    
+    Memory_getStats(NULL, &stats);
+    System_printf("heap size: 0x%x, free: 0x%x, max free block: 0x%x\n",
+                  stats.totalSize, stats.totalFreeSize, stats.largestFreeSize);
+    System_flush();
+
+    delay(50);                  // wait for 1/20 second
+    digitalWrite(ledPin, LOW);  // set the LED off
+    delay(950);                 // wait for (1 - 1/20) second
 }
