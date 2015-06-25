d4adbfccc4306655b91b263f1e89ee7917c8774b
dave russo <d-russo@ti.com>
2015-06-25 11:34:14 -0700

    add package.bld sanity test for cc2650

---
+++ src/ti/runtime/wiring/cc26xx/tests/sanity/blink.cpp
@@ -0,0 +1,16 @@
+#include <Energia.h> // Energia Wiring API
+
+int period = 1;
+
+__extern void blinkSetup(void)
+{
+    //    pinMode(RED_LED, OUTPUT);    // set ledPin pin as output
+}
+
+__extern void blinkLoop(void)
+{
+    //    digitalWrite(RED_LED, HIGH); // set the LED on
+    //    delay(period);               // wait for period ms
+    //    digitalWrite(RED_LED, LOW);  // set the LED off
+    //    delay(1000 - period);        // wait for remainder of 1 second
+}
---
+++ src/ti/runtime/wiring/cc26xx/tests/sanity/package.xdc
@@ -0,0 +1,6 @@
+/*
+ *  ======== ti.runtime.wiring.cc26xx.tests.sanity ========
+ *  Sanity test of the ti.runtime.wiring.cc26xx library
+ */
+package ti.runtime.wiring.cc26xx.tests.sanity {
+}
