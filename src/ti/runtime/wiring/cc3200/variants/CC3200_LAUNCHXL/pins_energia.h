0df530e43a8ea739020420c8c2c0e727abd3753d
Alan DeMars <ademars@ti.com>
2015-07-01 17:10:53 -0700

    add A0-A3 definitions for CC3200_LAUNCHXL

--- src/ti/runtime/wiring/cc3200/variants/CC3200_LAUNCHXL/pins_energia.h
+++ src/ti/runtime/wiring/cc3200/variants/CC3200_LAUNCHXL/pins_energia.h
@@ -43,4 +43,9 @@ static const uint8_t YELLOW_LED = 9;
 static const uint8_t PUSH1 = 3;
 static const uint8_t PUSH2 = 11;
 
+static const uint8_t A0 = 23;
+static const uint8_t A1 = 2;
+static const uint8_t A2 = 6;
+static const uint8_t A3 = 24;
+
 #endif
