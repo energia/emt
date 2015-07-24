867798f103c76b09e0189607dab3681c305d0e1d
Alan DeMars <ademars@ti.com>
2015-07-24 16:38:13 -0700

    Test case mods to test analogWrite()

--- src/ti/runtime/wiring/cc26xx/tests/alan/sketch7.cpp
+++ src/ti/runtime/wiring/cc26xx/tests/alan/sketch7.cpp
@@ -20,25 +20,23 @@ void buttonFxn1()
 
     state = (state + 1) & 0x7;
 
-    if (state & 0x1) {
-    	digitalWrite(RED_LED, 1);
-    }
-    else {
-    	digitalWrite(RED_LED, 0);
-    }
-
-    if (state & 0x2) {
-//    	digitalWrite(GREEN_LED, 1);
-    }
-    else {
-//    	digitalWrite(GREEN_LED, 0);
-    }
-
-    if (state & 0x4) {
-//    	digitalWrite(YELLOW_LED, 1);
-    }
-    else {
-//    	digitalWrite(YELLOW_LED, 0);
+    switch(state) {
+	case 0: 
+	case 4: 
+	    analogWrite(RED_LED, 0);
+	    break;
+	case 1:
+	case 5: 
+	    analogWrite(RED_LED, 100);
+	    break;
+	case 2:
+	case 6: 
+	    analogWrite(RED_LED, 200);
+	    break;
+	case 3:
+	case 7: 
+	    analogWrite(RED_LED, 255);
+	    break;
     }
 }
 
@@ -47,7 +45,8 @@ void setup7()
     pinMode(PUSH1, INPUT_PULLUP);
     pinMode(PUSH2, INPUT_PULLUP);
     pinMode(RED_LED, OUTPUT);
-    pinMode(GREEN_LED, OUTPUT);
+    pinMode(RED_LED, OUTPUT);
+//    pinMode(GREEN_LED, OUTPUT);
 
     attachInterrupt(PUSH1, buttonFxn0, RISING);
     attachInterrupt(PUSH2, buttonFxn1, RISING);
@@ -58,10 +57,12 @@ void setup7()
 
 void loop7()
 {
-//    digitalWrite(RED_LED, 1);
+    digitalWrite(RED_LED, 1);
 //    digitalWrite(GREEN_LED, 0);
     delay(1000);
 //    digitalWrite(RED_LED, 0);
 //    digitalWrite(GREEN_LED, 1);
     delay(1000);
+//    analogWrite(RED_LED, 100);
+//    analogWrite(GREEN_LED, 100);
 }
